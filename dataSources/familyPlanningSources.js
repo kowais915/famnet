3467a043ee4c5e4bc2060a8e8abebdcd12f30214
Owais Khan <kowais915@gmail.com>
2024-08-22 23:34:34 +0500

    🚀 Deployment

--- dataSources/familyPlanningSources.js
+++ dataSources/familyPlanningSources.js
@@ -1,20 +1,13 @@
 const familyPlanningSources = [
-    {
-        source: "Health.com",
-        url: "https://www.health.com/condition/birth-control",
+  {
+    source: "HuffPost",
+    url: "https://www.huffpost.com/news/topic/birth-control",
+  },
 
-    },
-    {
-        source: "HuffPost",
-        url: "https://www.huffpost.com/news/topic/birth-control"
-        
-    },
-
-    {
-        source: "BirthControl.com",
-        url: "https://birthcontrol.com/blog/"
-    }
-
+  {
+    source: "BirthControl.com",
+    url: "https://birthcontrol.com/blog/",
+  },
 ];
 
 module.exports = familyPlanningSources;
