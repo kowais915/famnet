ea8eafa78ef577b855878a127e1a0d2c7fdbbf4c
Owais Khan <kowais915@gmail.com>
2024-08-23 00:00:29 +0500

    💩 Dirty and hacky code that needs to be improved

--- api.js
+++ api.js
@@ -5,10 +5,13 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const womenHealth = require("./dataSources/womenHealth");
 const pregnancySource = require("./dataSources/pregnancy");
-const {getHealthContent, getPregContent, getFmilyPlanning} = require("./functions/functions")
+const {
+  getHealthContent,
+  getPregContent,
+  getFmilyPlanning,
+} = require("./functions/functions");
 const functions = require("./functions/functions");
 PORT = process.env.PORTAPI || 3002;
-
 
 //express app
 const app = express();
@@ -16,101 +19,74 @@ const app = express();
 //middleware
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // database and listening
-app.listen(process.env.PORT|| 3002, ()=>{
-    console.log("Lsitening to request at port 3000");
+app.listen(process.env.PORT || 3002, () => {
+  console.log("Lsitening to request at port 3000");
 });
-
-
 
 // FUNCTIONS
 //this function goes out and fetches health stuff
-getHealthContent()
-.then(val=>{
-    console.log("Health Content Fetched. Success!")
-})
+getHealthContent().then((val) => {
+  console.log("Health Content Fetched. Success!");
+});
 
 //pregnancy function
-getPregContent()
-.then(val=>{
-    console.log("Pregnancy data fetched. Success!");
-})
-
+getPregContent().then((val) => {
+  console.log("Pregnancy data fetched. Success!");
+});
 
 //family planning
-getFmilyPlanning()
-.then(result=>{
-    console.log("Family Planning content fetched. Success!")
-})
-
-
-
+getFmilyPlanning().then((result) => {
+  console.log("Family Planning content fetched. Success!");
+});
 
 // ROUTING STARTS
 //docs page
 
-app.get("/docs", (req, res)=>{
+app.get("/docs", (req, res) => {
+  // res.json({msg: "Welcome to the famnet documentation page for its API"})
+  res.render("docs");
+});
 
-    // res.json({msg: "Welcome to the famnet documentation page for its API"})
-    res.render("docs")
-})
-
-app.get("/", (req, res)=>{
-
-    res.json(
-        {msg: "Welcome to the famnet documentation page for its API ",
-        documentation: "https://famnet-grhqstk6u-kowais915s-projects.vercel.app/docs"})
-    
-})
-
+app.get("/", (req, res) => {
+  res.json({
+    msg: "Welcome to the famnet documentation page for its API ",
+    documentation:
+      "https://famnet-grhqstk6u-kowais915s-projects.vercel.app/docs",
+  });
+});
 
 //api route page
-app.get("/api", (req, res)=>{
-
-    res.json({msg: "Welcome to the famnet API"})
-})
-
-
+app.get("/api", (req, res) => {
+  res.json({ msg: "Welcome to the famnet API" });
+});
 
 //get all content about family planning and counselling
-app.get("/api/all", (req, res)=>{
-    res.json({msg: "Here is all the content for family planning and counseling"});
-
-
-})
+app.get("/api/all", (req, res) => {
+  res.json({
+    msg: "Here is all the content for family planning and counseling",
+  });
+});
 
 //get content from a source when the user enters the name of a source
-app.get("/api/:source", (req, res)=>{
-    
-    res.json({msg: `Here is the content from ${req.params.source}`})
-})
-
+app.get("/api/:source", (req, res) => {
+  res.json({ msg: `Here is the content from ${req.params.source}` });
+});
 
 //this end-point returns latest info about women's health
-app.get("/health", (req, res)=>{
-  
-    res.json(functions.healthArray);
-    
- 
-})
+app.get("/health", (req, res) => {
+  res.json(functions.healthArray);
+});
 
 //this end-point pregnany related content
 //this end-point returns latest info about women's health
-app.get("/pregnancy", (req, res)=>{
-  
-    res.json(functions.pregArry);
-    
- 
-})
+app.get("/pregnancy", (req, res) => {
+  res.json(functions.pregArry);
+});
 
 //this end-point returns birth control content
-app.get("/birth", (req, res)=>{
-  
-    res.json(functions.birthArray.reverse());
-    
- 
-})
-
-    
+app.get("/birth", (req, res) => {
+  res.json(functions.birthArray.reverse());
+});
